7962303e394b43613dbca4ebe4ba5c232e823c04
chaozlk <sousamurairtx@gmail.com>
2026-08-03 00:13:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-import './src/app.js';
+const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+const app = express();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once("ready", () => {
+  console.log(`Bot ${client.user.tag} aktif!`);
+  client.user.setPresence({
+    activities: [{ name: "Sunucuyu yönetiyor", type: 0 }],
+    status: "online"
+  });
+});
+
+client.on("messageCreate", message => {
+  if (message.content === "!ping") {
+    message.reply("Pong!");
+  }
+});
+
+const token = process.env.TOKEN;
+client.login(token);
+
+app.get("/", (req, res) => res.send("Bot çalışıyor!"));
+app.listen(3000, () => console.log("Web sunucusu aktif."));
